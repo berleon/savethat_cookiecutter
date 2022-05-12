5088015376fc9c7f0650a4cf833f160a4e21a453
berleon <github@leon-sixt.de>
2022-05-12 13:49:09 +0200

    set project dir in test

--- {{cookiecutter.project_slug}}/tests/conftest.py
+++ {{cookiecutter.project_slug}}/tests/conftest.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 
 import pytest
+import savethat
 
 
 @pytest.fixture
@@ -12,6 +13,8 @@ def env_file(tmp_path: Path) -> Path:
     Using this fixture ensure that local files are saved to the pytest tmpdir.
     """
 
+    test_dir = Path(__file__).parent
+    savethat.set_project_dir(test_dir)
     data_path = tmp_path / "data"
     with open(tmp_path / "savethat.toml", 'w') as f:
         print(f'local_path="${data_path}"', file=f)
